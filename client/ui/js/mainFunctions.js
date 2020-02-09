0a5afe8db3f68e13f3e478f70c806d2f8b44bee2
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-02-09 14:33:53 +0100

    Correctif interface
    Changement des chemins absolus en relatifs

--- client/ui/js/mainFunctions.js
+++ client/ui/js/mainFunctions.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".background-container").load("/common/background.html");
-  $(".board-container").load("/common/board.html");
-  $(".ingame-ui-container").load("/common/ingame_ui.html");
+  $(".background-container").load("common/background.html");
+  $(".board-container").load("common/board.html");
+  $(".ingame-ui-container").load("common/ingame_ui.html");
 });
