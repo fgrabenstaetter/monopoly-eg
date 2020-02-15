fc2d0aa9e5ccba081a37a51c12d1bc1f3374f914
Danyl EL-KABIR <elkabirdanyl@gmail.com>
2020-02-15 12:36:36 +0100

    Révision et correction de l'algo de fusion des lobbys + matchmaking

--- server/game/lobby.js
+++ server/game/lobby.js
@@ -1,18 +1,19 @@
+//const Chat = require('./chat.js');
+
 /**
  * Représente un Lobby
  */
 class Lobby {
-    constructor (id, nbPlayers, users, chat, maxPlayersNb, matchmaking) {
+    /**
+     * @param id ID unique pour le lobby
+     * @param user L'utilisateur qui veut créer le lobby
+     * @param matchmaking L'instance globale de matchmaking du serveur
+     */
+    constructor (id, user, matchmaking) {
         this.id = id;
-        this.nbPlayers = nbPlayers;
-        this.users = users;
-        this.chat = chat;
-        if (maxPlayersNb < 2)
-            this.maxPlayersNb = 2;
-        else if (maxPlayersNb > 8)
-            this.maxPlayersNb = 8;
-        else
-            this.maxPlayersNb = maxPlayersNb;
+        this.users = [user];
+        //this.chat = new Chat();
+        this.targetUsersNb = 4;
         // Il y aura un seul objet matchmaking qui sera un attribut de chaque objet Lobby pour simplifier
         this.matchmaking = matchmaking;
     }
@@ -33,19 +34,23 @@ class Lobby {
             this.users.splice(this.users.indexOf(user), 1);
     }
 
+    get nbUsers () {
+        return this.users.length;
+    }
+
     /**
      * @param newNb le nouveau nombre de joueur de la partie à chercher
      */
     changeMaxPlayersNb (newNb) {
         // Si on veut changer pour un nombre plus petit que 2 on le met automatiquement à 2
         if (newNb < 2)
-            this.maxPlayersNb = 2;
+            this.targetUsersNb = 2;
         // Si on veut changer pour un nombre plus grand que 8 on le met automatique à 8
         else if (newNb > 8)
-            this.maxPlayersNb = 8;
+            this.targetUsersNb = 8;
         // Sinon le nombre est correct
         else
-            this.maxPlayersNb = newNb;
+            this.targetUsersNb = newNb;
     }
 
     searchGame () {
--- server/game/matchmaking.js
+++ server/game/matchmaking.js
@@ -8,11 +8,16 @@ class Matchmaking {
         /** Tableau de 6 cases => 1 case pour chaque nbMax de joueurs souhaité par des lobby par    exemple
          * case indice 0 => Lobby pour une partie à 2 joueurs
          * case indice 1 => Lobby pour une partie à 3 joueurs
+         * case indice 2 => Lobby pour une partie à 4 joueurs
+         * case indice 3 => Lobby pour une partie à 5 joueurs
+         * case indice 4 => Lobby pour une partie à 6 joueurs
+         * case indice 5 => Lobby pour une partie à 7 joueurs
+         * case indice 6 => Lobby pour une partie à 8 joueurs
         */
         this.games = [];
-        this.playersWaiting = new Array(6);
+        this.playersWaiting = new Array(7);
         let i;
-        for (i = 0; i < 6; i++)
+        for (i = 0; i < 7; i++)
             this.playersWaiting[i] = [];
         setInterval(this.checkLaunch.bind(this), 1e3);
     }
@@ -22,13 +27,15 @@ class Matchmaking {
      */
     addLobby (lobby) {
         // On lance la partie directement si le nombre de joueurs correspond au nombre de joueurs attendus pour la partie
-        if (lobby.maxPlayersNb === lobby.nbPlayers) {
+        if (lobby.targetUsersNb === lobby.nbUsers) {
             // objet game à compléter après implémentation de la classe de ce dernier
-            const game = new Gameboard(lobby.users);
-            this.games.push(game);
+            /* const game = new Gameboard(lobby.users);
+            this.games.push(game);*/
+            console.log("YIKES");
         }
+        // Sinon on l'insère dans la bonne file d'attente
         else {
-            const index = lobby.maxPlayersNb - 2;
+            const index = lobby.targetUsersNb - 2;
             if (this.playersWaiting[index].indexOf(lobby) === -1)
                 this.playersWaiting[index].push(lobby);
         }
@@ -38,31 +45,44 @@ class Matchmaking {
      * @param lobby L'objet correspondant au lobby à supprimer
      */
     delLobby (lobby) {
-        const index = lobby.maxPlayersNb - 2;
+        const index = lobby.targetUsersNb - 2;
         if (this.playersWaiting[index].indexOf(lobby) !== -1)
             this.playersWaiting[index].splice(this.playersWaiting[index].indexOf(lobby), 1);
     }
 
     checkLaunch () {
-        let i, j, k;
-        for (i = 0; i < 6; i++) {
-            const nbMax = i+2;
-            for (j = 0; j < this.playersWaiting[i].length; j++) {
-                let sum = this.playersWaiting[i][j].nbPlayers;
-                for (k = 0; k < this.playersWaiting[i].length; k++) {
-                    let lobbyUnion = [];
-                    sum += this.playersWaiting[i][k].nbPlayers;
-                    if (sum === nbMax) {
-                        lobbyUnion = this.playersWaiting[i][j].users.concat(this.playersWaiting[i][k].users);
-                        this.delLobby(this.playersWaiting[i][j]);
-                        this.delLobby(this.playersWaiting[i][k]);
-                        console.log("HELLO WORLD!")
-                        //const game = new Gameboard(lobbyUnion);
-                        //this.games.push(game);
-                    }
-                    else if (sum > nbMax){
-                        sum -= this.playersWaiting[i][k].nbPlayers;
-                        console.log("Je passe mon tour!");
+        for (let i = 0; i < 7; i++) {
+            let nbMax = i+2;
+            for (let j = 0; j < this.playersWaiting[i].length; j++) {
+                let sum = this.playersWaiting[i][j].nbUsers;
+                let fusion = [];
+                for (let k = 0; k < this.playersWaiting[i].length; k++) {
+                    sum += this.playersWaiting[i][k].nbUsers;
+                    if (k !== j) {
+                        if (sum < nbMax) {
+                            if (fusion.indexOf(j) === -1)
+                                fusion.push(i);
+                            if (fusion.indexOf(k) === -1)
+                                fusion.push(j);
+                        }
+                        else if (sum > nbMax)
+                            sum -= this.playersWaiting[i][k].nbUsers;
+                        else {
+                            if (fusion.indexOf(j) === -1)
+                                fusion.push(j);
+                            if (fusion.indexOf(k) === -1)
+                                fusion.push(k);
+                            let mergedLobby = [];
+                            for (let f of fusion)
+                                mergedLobby = mergedLobby.concat(this.playersWaiting[i][f].users);
+
+                            for (let f of fusion) {
+                                // f < this.playersWaiting[i].length
+                                this.delLobby(this.playersWaiting[i][f]);
+                            }
+                            // Ici il faudra instancier un Gameboard et l'ajouter au tableau games de matchmaking, après l'implémentation de la classe Gameboard
+                            console.log("Lobbys fusionnés!");
+                        }
                     }
                 }
             }
@@ -70,19 +90,44 @@ class Matchmaking {
     }
 }
 
+// TEST de la fusion des lobbys -- A SUPPRIMER!
 let match = new Matchmaking(0);
-let lob1 = new Lobby(0, 1, [], {}, 6, match);
-let lob2 = new Lobby(1, 1, [], {}, 6, match);
-let lob3 = new Lobby(2, 1, [], {}, 6, match);
-let lob4 = new Lobby(3, 2, [], {}, 6, match);
-let lob5 = new Lobby(4, 5, [], {}, 6, match);
+let lob1 = new Lobby(0, {}, match);
+let lob2 = new Lobby(1, {}, match);
+let lob3 = new Lobby(2, {}, match);
+let lob4 = new Lobby(3, {}, match);
+let lob5 = new Lobby(4, {}, match);
+let lob6 = new Lobby(5, {}, match);
+lob1.addUser({});
+lob1.addUser({});
+lob1.addUser({});
+lob2.addUser({});
+lob3.addUser({});
+lob3.addUser({});
+lob3.addUser({});
+lob3.addUser({});
+lob3.addUser({});
+lob5.addUser({});
+lob5.addUser({});
+lob6.addUser({});
+lob6.addUser({});
+lob6.addUser({});
+lob6.addUser({});
+lob6.addUser({});
+lob1.changeMaxPlayersNb(5);
+lob2.changeMaxPlayersNb(5);
+lob3.changeMaxPlayersNb(5);
+lob4.changeMaxPlayersNb(5);
+lob5.changeMaxPlayersNb(5);
+lob6.changeMaxPlayersNb(6);
 match.addLobby(lob1);
 match.addLobby(lob2);
 match.addLobby(lob3);
 match.addLobby(lob4);
 match.addLobby(lob5);
-for (i = 0; i < 5; i++)
-    console.log(match.playersWaiting[i]);
-for (i = 0; i < 5; i++)
-    console.log(match.playersWaiting[i]);
-module.exports = Matchmaking;
+match.addLobby(lob6);
+console.log(lob1.nbUsers);
+console.log(lob2.nbUsers);
+console.log(lob3.nbUsers);
+console.log(lob4.nbUsers);
+console.log(lob5.nbUsers);
