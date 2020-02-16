8242021849f8ebcdf712987d3b34498396e6011f
Edouard Gu <e.gu@unistra.fr>
2020-02-16 14:47:31 +0100

    implementation du scroll du chat lors d'un nouveau message

--- client/ui/js/gChat.js
+++ client/ui/js/gChat.js
@@ -1,17 +1,29 @@
 document.getElementById('chat').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-		sendMsg();
-    }
+  if (e.key === 'Enter')
+    sendMsg();
 });
 document.getElementById('btnSendMsg').addEventListener("click", sendMsg);
 
 function sendMsg() {
 	if (document.getElementById('chat').value.trim()!="") {
-		msg = document.createElement('div');
+    const element = document.getElementById("msgChat");
+    const isScroll = element.scrollHeight - element.clientHeight <= element.scrollTop + 1;
+
+    msg = document.createElement('div');
 		msg.className = 'msg-me';
 		msg.innerHTML = document.getElementById('chat').value;
 		document.getElementById('chat').value="";
 		chat = document.getElementById('msgChat');
 		chat.appendChild(msg);
+    if (isScroll)
+      updateScroll();
 	}
+}
+
+function updateScroll(){
+  const element = document.getElementById("msgChat");
+  console.log(element.scrollTop);
+  console.log(element.scrollHeight);
+
+  element.scrollTop = element.scrollHeight;
 }
