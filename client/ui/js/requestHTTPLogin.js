aa573a8a1c10ae0fb7269adfcfc9fca41790ede8
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-02-16 18:17:24 +0100

    Correction d'une erreur de requête HTTP

--- client/ui/js/requestHTTPLogin.js
+++ client/ui/js/requestHTTPLogin.js
@@ -1,5 +1,3 @@
-let urlApi = 'https://monopolyegdev.singlequote.net/api'
-
 // Requête HTTP AJAX - Connexion
 $(document).ready(function() {
 
--- client/ui/js/requestHTTPSignin.js
+++ client/ui/js/requestHTTPSignin.js
@@ -1,5 +1,3 @@
-let urlApi = 'https://monopolyegdev.singlequote.net/api'
-
 // Requête HTTP AJAX - Inscription
 $(document).ready(function() {
     
