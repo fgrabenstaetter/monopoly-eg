c7c1ac38fc1e85996ed85180df5196de47e872af
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-02-16 11:50:53 +0100

    Completion JS chat

--- client/ui/js/gChat.js
+++ client/ui/js/gChat.js
@@ -1,17 +1,17 @@
 document.getElementById('chat').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-
+		sendMsg();
     }
 });
 document.getElementById('btnSendMsg').addEventListener("click", sendMsg);
 
 function sendMsg() {
-		if (document.getElementById('chat').value.trim()!="") {
-				msg = document.createElement('div');
-				msg.className = 'msg-me';
-				msg.innerHTML = document.getElementById('chat').value;
-				document.getElementById('chat').value="";
-				chat = document.getElementById('msgChat');
-				chat.appendChild(msg);
-		}
+	if (document.getElementById('chat').value.trim()!="") {
+		msg = document.createElement('div');
+		msg.className = 'msg-me';
+		msg.innerHTML = document.getElementById('chat').value;
+		document.getElementById('chat').value="";
+		chat = document.getElementById('msgChat');
+		chat.appendChild(msg);
+	}
 }
