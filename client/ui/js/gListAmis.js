63d777e6ae900004d88c1d616caaae10e2c9cc07
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-02-19 19:45:27 +0100

    Ajout de la liste d'amis à l'UI

--- client/ui/js/gListAmis.js
+++ client/ui/js/gListAmis.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
   $('#friendBar').keyup(function(e) {
-    let input, filter, ul, li, a, i, txtValue;
+    let input, filter, element, a, i, txtValue;
     input = document.getElementById("friendBar");
     filter = input.value.toUpperCase();
     console.log(filter);
-    ul = document.getElementById("friendList");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        txtValue = li[i].innerHTML;
+    element = document.getElementsByClassName("friend-entry");
+    for (i = 0; i < element.length; i++) {
+        txtValue = element[i].innerHTML;
         if (txtValue.toUpperCase().indexOf(filter) > -1)
-            li[i].style.display = "";
+            element[i].style.display = "";
         else
-            li[i].style.display = "none";
+            element[i].style.display = "none";
     }
-}
+    }
+  )})
