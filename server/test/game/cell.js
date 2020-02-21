a6d19c3691af56aed9dad5b758503660cd5381e4
Danyl EL-KABIR <elkabirdanyl@gmail.com>
2020-02-21 10:43:19 +0100

    test

--- server/test/game/cell.js
+++ server/test/game/cell.js
@@ -3,12 +3,37 @@ const Property = require('../../game/property');
 const PublicCompany = require('../../game/publicCompany');
 const Street = require('../../game/street');
 const Constants = require('../../lib/constants');
+const Properties = require('../../lib/properties');
+const User = require('../../game/user');
 const assert = require('assert');
 
 describe("Test sur la classe Cell", function() {
     it('Doit renvoyer/afficher Parc', function() {
-        let proCell0 = new Street(0);
-        let testParc = new Cell(0, Constants.CELL_TYPE.PARC, proCell0);
+        const userSchema1 = {
+            nickname: 'François',
+            email: 'francois@gmail.com',
+            friends: ['Danyl', 'Boris', 'Matthias'],
+            inscriptionDatetime: 152888912,
+            level: 1,
+            exp: 0
+        };
+        const userSchema2 = {
+            nickname: 'Danyl',
+            email: 'danyl@gmail.com',
+            friends: ['François', 'Matthias', 'Boris'],
+            inscriptionDatetime: 152888912,
+            level: 1,
+            exp: 0
+        };
+
+        const user1 = new User(userSchema1);
+        const user2 = new User(userSchema2);
+
+        //3e arg à compléter lorsque la classe Game sera prête à l'emploi
+        let proCell1 = new Street(0, user1, {});
+        //3e arg à compléter lorsque la classe Game sera prête à l'emploi
+        let proCell2 = new Street(1, user2, {});
+        let testParc = new Cell(0, Constants.CELL_TYPE.PARC, proCell1);
         assert.equal('Parc', testParc.name);
     });
 });
