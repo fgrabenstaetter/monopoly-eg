eb9821920e8bd9072b665d271b2145e546f14653
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-02-23 20:02:36 +0100

    Mise à jour des URL (config.js)

--- client/ui/js/config.js
+++ client/ui/js/config.js
@@ -1,8 +1,8 @@
 // DEV LOCAL
-const urlApi = 'http://localhost:3000/api';
-const socketUrl = 'http://localhost:3000';
+// const urlApi = 'http://localhost:3000/api';
+// const socketUrl = 'http://localhost:3000';
 
-// const urlApi = 'https://monopolyegdev.singlequote.net/api';
-// const socketUrl = 'https://monopolyegdev.singlequote.net';
+const urlApi = 'https://monopolyegdev.singlequote.net/api';
+const socketUrl = 'https://monopolyegdev.singlequote.net';
 
 const jwt = localStorage.getItem('jwt');
