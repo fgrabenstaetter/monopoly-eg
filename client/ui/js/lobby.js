c4cd3844e8c6cbe3c48480cce220b9ffdc4f5f49
Edouard Gu <e.gu@unistra.fr>
2020-02-24 17:07:26 +0100

    remise en forme du code

--- client/ui/js/lobby.js
+++ client/ui/js/lobby.js
@@ -7,9 +7,9 @@ socket.on('lobbyJoinedRes', (res) => {
 });
 
 socket.on('lobbyChatReceiveRes', (msg) => {
-    const element = document.getElementById("msgChat");
+    const element = document.getElementById('msgChat');
     const isScroll = element.scrollHeight - element.clientHeight <= element.scrollTop + 1;
-    let html = `<div class="msg-other"><div class="msg-author">` + msg.sender + `</div>` + msg.content + `</div>`;
+    let html = `<div class='msg-other'><div class='msg-author'>` + msg.sender + `</div>` + msg.content + `</div>`;
     $('#msgChat').append(html);
     if (isScroll)
         updateScroll();
@@ -20,17 +20,15 @@ $(document).ready( () => {
     updateScroll();
     $('#friendBar').keyup((e) => {
         let input, filter, element, a, i, txtValue;
-        input = document.getElementById("friendBar");
+        input = document.getElementById('friendBar');
         filter = input.value.toUpperCase();
-        console.log(filter);
-        element = document.getElementsByClassName("friend-entry");
+        element = document.getElementsByClassName('friend-entry');
         for (i = 0; i < element.length; i++) {
             txtValue = element[i].getElementsByClassName('friends-name')[0].innerHTML;
-            if (txtValue.toUpperCase().indexOf(filter) > -1){
-                element[i].style.display = "";
-            }
+            if (txtValue.toUpperCase().indexOf(filter) > -1)
+                element[i].style.display = '';
             else
-                element[i].style.display = "none";
+                element[i].style.display = 'none';
         }
     });
 
@@ -53,20 +51,20 @@ $(document).ready( () => {
     $('#rightNbJ').click( () => {
         rightNbJ();
     });
-})
+});
 
 /** Fonction qui affiche le message envoyer par le joueur sur le chat
 */
 function sendMsg() {
     let chatMsg = document.getElementById('chat').value;
 
-    if (chatMsg.trim()!="") {
+    if (chatMsg.trim()!='') {
         let msg = document.createElement('div');
         msg.className = 'msg-me';
 
         let msgAuthor = document.createElement('div');
         msgAuthor.className = 'msg-author';
-        msgAuthor.innerHTML = `<div class="msg-author">Moi</div>`;
+        msgAuthor.innerHTML = `<div class='msg-author'>Moi</div>`;
 
         let msgContent = document.createElement('div');
         msgContent.innerText = chatMsg;
@@ -74,7 +72,7 @@ function sendMsg() {
         msg.appendChild(msgAuthor);
         msg.appendChild(msgContent);
 
-        document.getElementById('chat').value="";
+        document.getElementById('chat').value='';
         chat = document.getElementById('msgChat');
 
         socket.emit('lobbyChatSendReq', {content: chatMsg});
@@ -85,7 +83,7 @@ function sendMsg() {
 /** Fonction qui remet la barre de défilement en bas
 */
 function updateScroll(){
-    const element = document.getElementById("msgChat");
+    const element = document.getElementById('msgChat');
     element.scrollTop = element.scrollHeight;
 }
 
