285a71c21fddcae6ffe3e3f733044d4aee1a5b1e
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-03-03 16:02:25 +0100

    Màj de l'esthétique des notifications en jeu

--- client/ui/js/populateBtn.js
+++ client/ui/js/populateBtn.js
@@ -8,7 +8,7 @@ $('.avatar-bubble').text('CHANGER DE PION');
 
 // Ingame
 /* Pour les notifications d'achat */
-$('.ingame-ui-container .sale .subtitle').text('À VENDRE');
+$('.ingame-ui-container .sale .state').text('À VENDRE');
 $('.ingame-ui-container .sale .btn-primary').text('ACHETER');
 $('.ingame-ui-container .sale .btn-secondary').text('NE RIEN FAIRE');
 
