332b397090beb8a0b545bdd36442e0fc82852545
Boris Flesch <boris.flesch@gmail.com>
2020-03-17 14:37:26 +0100

    getter pour la liste d'amis dans le model User

--- server/game/user.js
+++ server/game/user.js
@@ -12,7 +12,6 @@ class User {
         this.id = userSchema.id;
         this.nickname = userSchema.nickname;
         this.email    = userSchema.email;
-        this.friends  = userSchema.friends;
         this.level    = userSchema.level;
         this.exp      = userSchema.exp;
         this.inscriptionDatetime = userSchema.inscriptionDatetime;
@@ -21,6 +20,23 @@ class User {
 
         this.levelUpExp = 100;
         this.socket = null;
+    }
+
+    /**
+     * Renvoie les amis (tableau d'ids) de User
+     */
+    get friends() {
+        let friends = [];
+
+        UserSchema.getAcceptedFriends(this.id, (error, friendships) => {
+            if (!friendships || error || friendships.length == 0)
+                return [];
+
+            for (let i = 0; i < friendships.length; i++)
+                friends.push(friendships[i].friend._id);
+            
+            return friends;
+        });
     }
 
     /**
