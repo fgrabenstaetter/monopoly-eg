a4b77898a3136a9ed6a7213c30d5c01ad716044d
Danyl EL-KABIR <elkabirdanyl@gmail.com>
2020-03-18 16:51:33 +0100

    add mocket-io

--- server/test/game/network.js
+++ server/test/game/network.js
@@ -1,42 +1,10 @@
-const ioClient    = require('socket.io-client');
-const assert      = require('assert')
-const app         = require('express')();
-const http        = require('http');
-const Constants   = require('../../lib/constants');
-const Errors      = require('../../lib/errors');
-const User        = require('../../game/user');
-const Lobby       = require('../../game/lobby');
-const Matchmaking = require('../../game/matchmaking');
-const Network     = require('../../game/network');
+const mocket = require("mocket-io");
+const assert = require('assert');
 
-describe('Network + sockets', () => {
+const server = new mocket.Server();
+const client = new mocket.Client(server);
 
-    const port = 3002;
-    const server = http.createServer(app).listen(port);
-    const ioServer = require('socket.io')(server);
-    let clientSocket, serverSocket;
-    let user = new User({
-        id                  : 12489324,
-        nickname            : 'Danyl',
-        email               : 'danyl@gmail.com',
-        level               : 2,
-        exp                 : 0,
-        inscriptionDatetime : 834823492323
-    });
-
-    let GLOBAL = {
-        users   : [],
-        lobbies : [],
-        games   : []
-    };
-    GLOBAL.matchmaking = new Matchmaking(GLOBAL);
-    GLOBAL.network     = new Network(ioServer, GLOBAL);
-
-    ioServer.on('connection', (sock) => {
-        serverSocket = sock;
-        user.socket = serverSocket;
-        sock.emit('YES');
-    });
+let clientSocket, serverSocket;
 
     after( () => {
         process.exit(0);
@@ -57,13 +25,6 @@ describe('Network + sockets', () => {
         clientSocket.close();
         done();
     });
-
-    it('Réception de lobbyCreatedRes par l\'hôte', (done) => {
-        const lobby = new Lobby(user, GLOBAL);
-        GLOBAL.lobbies.push(lobby);
-        clientSocket.on('lobbyCreatedRes', (data) => {
-            done();
-        });
-        clientSocket.emit('lobbyReadyReq');
+        clientSocket.emit('message', 'Hello World!');
     });
 });
