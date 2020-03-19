cbba93d32b66055e6cd388e51d324429d34f225d
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-03-19 11:21:58 +0100

    Import fichiers threejs dans ui

---
+++ client/ui/js/threejs/main.js
@@ -0,0 +1,16 @@
+loaderPawn('tracteur');
+loaderPawn('boat');
+loaderPawn('moto');
+loaderPawn('camion');
+
+loaderHouseProperty('M1_1_1');
+loaderHouseProperty('M1_1_2');
+loaderHouseProperty('M1_1_3');
+loaderHouseProperty('M1_1_4');
+
+let scene = getScene();
+let camera = getCamera();
+let light = getLight(scene);
+let renderer = getRenderer();
+
+render();
