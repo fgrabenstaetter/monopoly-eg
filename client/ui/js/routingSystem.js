7b72f231a37a4f11a5624fa744e07b876c971ba7
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-03-19 10:57:17 +0100

    Réorganisation fonctions js UI

--- client/ui/js/routingSystem.js
+++ client/ui/js/routingSystem.js
@@ -1,3 +1,16 @@
+function loadContent () {
+        $(".background-container").load("html/background.html");
+        $(".board-container").load("html/board.html");
+        $(".ingame-ui-container").load("html/ingame_ui.html");
+        $(".lobby-ui-container").load("html/lobby_ui.html");
+        $(".options-overlay-container").load("html/options_overlay.html");
+        $(".profile-overlay-container").load("html/profile_overlay.html");
+        $(".resolution-overlay-container").load("html/res_overlay.html");
+        $(".login-ui-container").load("html/login_ui.html");
+        $(".signin-ui-container").load("html/signin_ui.html");
+}    
+
+// Routing des pages
 let contentDiv = document.getElementById('content');
 
 let homePage = `
