bf714ca4b8d299498d195a11bbc92010b491f6f4
Edouard Gu <e.gu@unistra.fr>
2020-03-21 02:24:57 +0100

    suppression d'une propriete

--- client/ui/js/popupsHandler.js
+++ client/ui/js/popupsHandler.js
@@ -41,40 +41,46 @@ function initProperty(){
     });
 }
 
-function createProperty(playerID, type, roadName){
+function createProperty(playerID, type, roadName, roadID){
     if (roadName == 'Eau') {
-        html =  `<div class="property eau">`
+        html =  `<div class="property eau data-id="` + roadID + `">`
                     + roadName +
                 `</div>`;
     }
     else if (roadName == 'Électricité') {
-        html =  `<div class="property electricite">`
+        html =  `<div class="property electricite" data-id="` + roadID + `">`
                     + roadName +
                 `</div>`;
     }
     else {
-        html =  `<div class="property">`
+        html =  `<div class="property" data-id="` + roadID + `">`
                     + roadName +
                 `</div>`;
     }
-    $('[data-id = "' + playerID + '"]').find('.' + type).append(html);
+    $('.player-entry[data-id = "' + playerID + '"]').find('.' + type).append(html);
 }
 
+
+function delProperty(roadID){
+    $('.property[data-id = "' + roadID + '"]').remove();
+}
+
+
 initProperty()
-createProperty('1', 'yellow', 'Avenue des Vosges');
-createProperty('1', 'yellow', 'Rue de la rue');
-createProperty('3', 'red', 'Avenue des Vosges');
-createProperty('1', 'blue', 'Avenue originale');
-createProperty('1', 'blue', 'Rue de la forêt');
-createProperty('2', 'blue', 'Rue de la ville');
-createProperty('1', 'blue', 'Rue étrange');
-createProperty('1', 'station', 'Université');
-createProperty('1', 'station', 'Homme de Fer');
-createProperty('1', 'company', 'Eau');
-createProperty('1', 'company', 'Électricité');
-createProperty('6', 'orange', 'Avenue des Vosges');
-createProperty('1', 'pink', 'Rue de la rue');
-createProperty('1', 'cyan', 'Avenue des Vosges');
-createProperty('4', 'green', 'Avenue originale');
-createProperty('4', 'green', 'Rue de la forêt');
-createProperty('4', 'brown', 'Rue de la ville');
+createProperty('1', 'yellow', 'Avenue des Vosges', 2);
+createProperty('1', 'yellow', 'Rue de la rue', 3);
+createProperty('3', 'red', 'Avenue des Vosges', 4);
+createProperty('1', 'blue', 'Avenue originale', 5);
+createProperty('1', 'blue', 'Rue de la forêt', 1);
+createProperty('2', 'blue', 'Rue de la ville', 8);
+createProperty('1', 'blue', 'Rue étrange', 9);
+createProperty('1', 'station', 'Université', 10);
+createProperty('1', 'station', 'Homme de Fer', 12);
+createProperty('1', 'company', 'Eau', 15);
+createProperty('1', 'company', 'Électricité', 20);
+createProperty('6', 'orange', 'Avenue des Vosges', 21);
+createProperty('1', 'pink', 'Rue de la rue', 32);
+createProperty('1', 'cyan', 'Avenue des Vosges', 23);
+createProperty('4', 'green', 'Avenue originale', 22);
+createProperty('4', 'green', 'Rue de la forêt', 31);
+createProperty('4', 'brown', 'Rue de la ville', 33);
