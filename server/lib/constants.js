32a455df1bced994ec276f73f74734831b2952e1
François Grabenstaetter <francoisgrabenstaetter@gmail.com>
2020-03-22 15:27:27 +0100

    Lancement dés auto si AFK ou déconnecté

--- server/lib/constants.js
+++ server/lib/constants.js
@@ -24,7 +24,7 @@ const Constants = {
     },
     GAME_PARAM: {
         WAITING_TIME_AFTER_READY : 2e3,
-        TURN_MAX_DURATION        : 60e3, // 60 secondes
+        TURN_MAX_DURATION        : 4e4,
         EXIT_JAIL_PRICE          : 50,
         GET_MONEY_FROM_START     : 200,
         BANK_INITIAL_MONEY       : 4000,
