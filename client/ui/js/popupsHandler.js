8fb3bfd604ee357d55a883166ea7b71f42a461c6
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-04-18 15:31:57 +0200

    Amélioration popup enchères

--- client/ui/js/popupsHandler.js
+++ client/ui/js/popupsHandler.js
@@ -190,8 +190,8 @@ function openBidPopup(id, playername, streetname) {
             `<div class="bid-popup" data-bidID="` + id + `">
             <div class="content">` + playername + ` lance une enchère pour ` + streetname + `</div>
             <div class="bid-input">
-                <input class="bid-input" type="text" placeholder="Entrez votre prix ici..."></input>
-                <button class="bid-validation" onclick="validateBid(` + id + `)">Valider</button>
+                <input class="bid-input" type="text" placeholder="Prix"></input>€
+                <button class="bid-validation" onclick="validateBid(` + id + `)">Enchérir</button>
             </div>
         </div>`;
     }
