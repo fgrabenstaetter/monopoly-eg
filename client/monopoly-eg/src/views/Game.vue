95d208321b138670e46087327c2efaabc0f5876b
FOYER MATTHIAS <matthias.foyer@etu.unistra.fr>
2020-04-29 16:11:10 +0200

    Correction sass + DOM

--- client/monopoly-eg/src/views/Game.vue
+++ client/monopoly-eg/src/views/Game.vue
@@ -115,13 +115,13 @@
                     </div>
                   </div>
 
-                  <div class="sortie-parlement card notification">
-                    <div class="card-body">
+                  <div id="sortie-parlement" class="card notification">
+                    <div class="card-body" :class="{'no-header': !notif.title}">
                       <div class="col-md-12 text-center value">
                         <p>Voulez-vous utiliser votre bonus "Sortir du Parlement" ?</p>
                       </div>
-                      <button class="btn btn-primary">Oui</button>
-                      <button class="btn btn-primary">Non</button>
+                      <button class="btn btn-primary accept-btn">Oui</button>
+                      <button class="btn btn-primary deny-btn">Non</button>
                     </div>
                   </div>
                 </div>
