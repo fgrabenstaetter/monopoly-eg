ca2a3b9d9094646b5605df1a8ab95253c779cd61
Boris Flesch <boris.flesch@gmail.com>
2020-05-01 14:28:31 +0200

    Webpack config retiré de vue.config.js

--- client/monopoly-eg/vue.config.js
+++ client/monopoly-eg/vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
     publicPath: '/',
-    configureWebpack: config => {
-        return {
-            plugins: [
-                new webpack.DefinePlugin({
-                'APPLICATION_VERSION': JSON.stringify(require('./package.json').version),
-                })
-            ]
-        }
-    },
     pluginOptions: {
         electronBuilder: {
             builderOptions: {
