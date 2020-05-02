805ed65848ee89cafcd894adb8576a6886233b7e
Boris Flesch <boris.flesch@gmail.com>
2020-05-02 20:03:05 +0200

    Inversion maker/receiver message chat offer

--- server/game/offer.js
+++ server/game/offer.js
@@ -93,7 +93,7 @@ class Offer {
 
         // message à tous les joueurs
         const propertyName = this.property ? 'la propriété ' + this.property.name : 'une carte Fin de session parlementaire';
-        const text = this.maker.nickname + ' a refusé l\'offre d\'achat pour ' + propertyName + ' de ' + this.receiver.nickname + ' à ' + this.amount + '€ !';
+        const text = this.receiver.nickname + ' a refusé l\'offre d\'achat pour ' + propertyName + ' de ' + this.maker.nickname + ' à ' + this.amount + '€ !';
         const mess = this.game.chat.addMessage(null, text);
         this.game.GLOBAL.network.io.to(this.game.name).emit('gameChatReceiveRes', {
             playerID: -1,
