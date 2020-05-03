dd3a3f2596f792f170357fcac1251f0c4b6af037
Edouard Gu <e.gu@unistra.fr>
2020-05-03 15:22:23 +0200

    ajout de la touche entrer

--- client/monopoly-eg/src/views/Game.vue
+++ client/monopoly-eg/src/views/Game.vue
@@ -912,7 +912,7 @@ export default {
     isNumber(evt) {
       evt = (evt) ? evt : window.event;
       var charCode = (evt.which) ? evt.which : evt.keyCode;
-      if (charCode < 48 || charCode > 57)
+      if ((charCode < 48 || charCode > 57) && charCode != 13)
         evt.preventDefault();
     }
   },
