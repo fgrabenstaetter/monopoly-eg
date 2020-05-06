7ba5d6090c8b9d0cc5fa309a25c705e6ce65b154
Boris Flesch <boris.flesch@gmail.com>
2020-05-06 16:20:31 +0200

    Electron check OK.

--- client/monopoly-eg/src/background.js
+++ client/monopoly-eg/src/background.js
@@ -47,10 +47,6 @@ function createWindow () {
   })
 }
 
-app.on('browser-window-created', (e, window) => {
-  window.setMenu(null);
-});
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
