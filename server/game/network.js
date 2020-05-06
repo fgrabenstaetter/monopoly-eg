5d3e8e4de9cf79097a904a6bbf30f657ae0d64a9
Danyl EL-KABIR <elkabirdanyl@gmail.com>
2020-05-06 18:30:07 +0200

    retour sur le broadcast

--- server/game/network.js
+++ server/game/network.js
@@ -306,15 +306,11 @@ class Network {
         });
 
         // envoyer à tous les users du loby, sauf le nouveau
-        for (const usr of lobby.users) {
-            if (usr !== user) {
-                usr.socket.emit('lobbyUserJoinedRes', {
-                    id       : user.id,
-                    nickname : user.nickname,
-                    avatar   : User.getAvatar(user.id)
-                });
-            }
-        }
+        user.socket.broadcast.emit('lobbyUserJoinedRes', {
+            id       : user.id,
+            nickname : user.nickname,
+            avatar   : User.getAvatar(user.id)
+        });
     }
 
     lobbyInvitationReq(user, lobby) {
